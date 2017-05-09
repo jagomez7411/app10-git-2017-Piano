c3bf47bf36b3ed2f13807f70df2b7c503a2b0ed4
jagomez7411 <aredcbtis16@hotmail.com>
2017-05-09 13:24:09 -0500

    7s

--- js/acciones.js
+++ js/acciones.js
@@ -3,15 +3,18 @@ $(document).ready(function(e) {
 	$('#principal').height($('#page1').height());
 	document.addEventListener("deviceready",function(){
 		
-		audio=window.plugins.NativeAudio;
+	//	audio=window.plugins.NativeAudio;
+	    window.plugins.NativeAudio.preloadSimple( 'do', 'audio/DO.mp3', function(){
+    }, function(e){alert( 'error: ' + e);
+    });
 		
-		audio.preloadFX('do','audio/DO.mp3',function(){},function(e){alert('Error '+e);});
-		audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error '+e);});
-		audio.preloadFX('mi','audio/MI.mp3',function(){},function(e){alert('Error '+e);});
-		audio.preloadFX('fa','audio/FA.mp3',function(){},function(e){alert('Error '+e);});
-		audio.preloadFX('sol','audio/SOL.mp3',function(){},function(e){alert('Error '+e);});
-		audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error '+e);});
-		audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('do','audio/DO.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('mi','audio/MI.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('fa','audio/FA.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('sol','audio/SOL.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error '+e);});
+		//audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error '+e);});
 		
 		$('.nota').bind('touchstart',function(){
 			$(this).addClass('tocada');
