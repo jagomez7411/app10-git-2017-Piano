4a1454654b257c36548660cb4346f22b19367591
jagomez7411 <aredcbtis16@hotmail.com>
2018-02-07 11:53:14 -0600

    14s

--- js/acciones.js
+++ js/acciones.js
@@ -1,38 +1,28 @@
 //JavaScript
-$(document).ready(function(e) {
-	$('#principal').height($('#page1').height());
-	document.addEventListener("deviceready",function(){
-		
-	//	audio=window.plugins.NativeAudio;
-	if( window.plugins && window.plugins.NativeAudio ) {
-	    window.plugins.NativeAudio.preloadSimple( 'do', 'audio/DO.mp3', function(){
-    }, function(e){alert( 'error: ' + e);
-    });
-	
-  window.plugins.NativeAudio.preloadComplex( 're', 'audio/RE.mp3', function(){
-    }, function(e){alert( 'error: ' + e);
-    });	
-	
-	   // Play 
-    window.plugins.NativeAudio.play( 'do' );
-    window.plugins.NativeAudio.loop( 're' );
-	
-		//audio.preloadFX('do','audio/DO.mp3',function(){},function(e){alert('Error '+e);});
-		//audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error '+e);});
-		//audio.preloadFX('mi','audio/MI.mp3',function(){},function(e){alert('Error '+e);});
-		//audio.preloadFX('fa','audio/FA.mp3',function(){},function(e){alert('Error '+e);});
-		//audio.preloadFX('sol','audio/SOL.mp3',function(){},function(e){alert('Error '+e);});
-		//audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error '+e);});
-		//audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error '+e);});
-	}
-		$('.nota').bind('touchstart',function(){
-			$(this).addClass('tocada');
-			audio.play($(this).attr('id'));
-			
-		}).bind('touchend',function(){
-			$(this).removeClass('tocada');
-		});
-		
-	},false);
-    
-});
+var audio;	
+$(document).ready(function(){	
+"use strict";
+  document.addEventListener("deviceready",function(){
+		audio= window.plugins.LowLatencyAudio; //construye la variable audio a un objeto
+    	//audio.preloadFX('id', 'ubicacion', que se hace si carga Bien el sonido, que voy a hacer si carga mal el sonido);
+		audio.preloadFX('DO','audio/DO.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('RE','audio/RE.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('MI','audio/MI.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('FA','audio/FA.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('SOL','audio/SOL.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('LA','audio/LA.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('SI','audio/SI.mp3', function(){}, function(e){alert('Error ' + e);});
+		$('.nota').bind('touchstart', function(){//bind escucha cualquier evento, 'touchstart'para donde den tap en la nota
+			$(this).addClass('tocada');//funcionalidad css que agrega clase
+			audio.play($(this).attr('id'));//play()reproducir la nota, $(this).attr('id') el atributo de quien lo invoco y lo reproduce	
+			}).bind('touchend',function(){//cierra nota .bind
+					$(this).removeClass('tocada');
+				});//bind
+				$('.nota1').bind('touchstart', function(){
+					$(this).addClass('tocada');
+					audio.play($(this).attr('id'));
+				}).bind('touchend',function(){
+					$(this).removeClass('tocada');
+				});		
+	},false); //deviceready
+});//ready
